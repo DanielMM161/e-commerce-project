f6dc4506a16fd2694e47b89cc8b898afba0ea029
Daniel Moreno Marquez <39881888+DanielMM161@users.noreply.github.com>
2022-12-22 18:17:06 +0100

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/Home'
+import LoginPage from './pages/Login'
+import ProductsIdPage from './pages/ProductId'
+import ProductsPage from './pages/Products'
+import ProfilePage from './pages/Profile'
+import RegisterPage from './pages/Register'
 
 const App = () => {
   return (
-    <div>App </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<ProductsPage />} />
+        <Route path="/product/:id" element={<ProductsIdPage />} />
+        <Route path="/Login" element={<LoginPage/>} />
+        <Route path="/Register" element={<RegisterPage />} />
+        <Route path="/Profile" element={<ProfilePage />} />
+      </Routes>
+  </BrowserRouter>
   )
 }
 
---
+++ src/pages/ProductId/index.tsx
@@ -0,0 +1,7 @@
+const ProductsIdPage = () => {
+    return (
+        <div>Products ID Page</div>
+    )
+}
+
+export default ProductsIdPage
